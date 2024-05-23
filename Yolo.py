864c35beb42e36317aeaf2eec5ffcaec38ad83d4
Harilegawa <120030681+WenaHarle@users.noreply.github.com>
2024-05-24 06:47:40 +0700

    Update Yolo.py

--- Yolo.py
+++ Yolo.py
@@ -1,44 +1,50 @@
+from ultralytics import YOLO
 import cv2
 
-# Initialize the first webcam
-cap1 = cv2.VideoCapture(2)
-cap1.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-cap1.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+# Load YOLO model
+model = YOLO('best.pt')
 
-# Initialize the second webcam
-cap2 = cv2.VideoCapture(4)
-cap2.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-cap2.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+# Initialize video capture for two cameras
+cap1 = cv2.VideoCapture(4)
+cap2 = cv2.VideoCapture(2)
 
-# Define the codec and create VideoWriter object for the first webcam
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Use 'mp4v' codec for mp4 format
-out1 = cv2.VideoWriter('last.mp4', fourcc, 30.0, (1920, 1080))
+# Set desired width and height for both cameras
+width = 1920  # Desired width
+height = 1080  # Desired height
 
-# Define the codec and create VideoWriter object for the second webcam
-out2 = cv2.VideoWriter('last.mp4', fourcc, 30.0, (1920, 1080))
+cap1.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+cap1.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
-while(cap1.isOpened() and cap2.isOpened()):
+cap2.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+cap2.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+
+while True:
     ret1, frame1 = cap1.read()
     ret2, frame2 = cap2.read()
+    
+    if ret1:
+        # Detect objects and track on frame1
+        results1 = model.track(frame1, persist=True)
+        frame1_ = results1[0].plot()
+        
+        # Visualize frame1
+        cv2.imshow('Camera 1', frame1_)
 
-    if ret1 and ret2:
-        # Write the frames to the output video files
-        out1.write(frame1)
-        out2.write(frame2)
+    if ret2:
+        # Detect objects and track on frame2
+        
+        results2 = model.track(frame2, persist=True)
+        frame2_ = results2[0].plot()
+        frame2_ = frame2_[200:800, 600:1200]
 
-        # Display the frames
-        cv2.imshow('frame1',frame1)
-        cv2.imshow('frame2',frame2)
+        # Visualize frame2
+        cv2.imshow('Camera 2', frame2_)
 
-        # Press 'q' to exit
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    else:
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release everything when done
+# Release video captures and close windows
 cap1.release()
 cap2.release()
-out1.release()
-out2.release()
 cv2.destroyAllWindows()
